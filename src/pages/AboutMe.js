36225e2ca37ecc6a8b54e6ef46199ca6e9a2d561
github username <github email address>
2020-08-22 20:12:38 -0500

    About me started

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function MyApps() {
+
+  return (
+    <div>
+      <p>Hi, I'm Sam Zandi, welcome to my portfolio. Some things to know about me:</p>
+      <ul>
+          <li>I am a graduate from the University of Illinois at Urbana-Champaign with a Bachelor of Science in Engineering Physics</li>
+      </ul>
+    </div>
+  );
+}
