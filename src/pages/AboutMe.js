4d39e1d0958f78df280b51df32e382e6a8dd913b
github username <github email address>
2020-08-29 19:02:14 -0500

    Removed changes from AboutMe

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -7,8 +7,6 @@ function MyApps() {
       <p>Hi, I'm Sam Zandi, welcome to my portfolio. Some things to know about me:</p>
       <ul>
           <li>I am a graduate from the University of Illinois at Urbana-Champaign with a Bachelor of Science in Engineering Physics</li>
-          <li>No ones going to look at this anyway</li>
-          <li>I'm never going to get hired</li>
       </ul>
     </div>
   );
